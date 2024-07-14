cfd345b577d1c2ae34bf87fb64e2cedd04fa00c2
박우림 <woopark@gmarket.com>
2024-07-14 16:10:29 +0900

    feature/some-random-game: 썸 랜덤 메시지 기능 완료

---
+++ src/apis/origin-data/ice-breaking-game/random-game/some-items.js
@@ -0,0 +1,167 @@
+export const someItems = [
+  {
+    topic: "꿈과 목표",
+    contents: [
+      { text: "장기적인 꿈", isChecked: false },
+      { text: "올해의 목표", isChecked: false },
+      { text: "최근에 달성한 성취", isChecked: false },
+      { text: "꿈을 향한 도전", isChecked: false },
+      { text: "인생의 롤모델", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "어린 시절 추억",
+    contents: [
+      { text: "어린 시절 가장 좋아했던 활동", isChecked: false },
+      { text: "가장 기억에 남는 생일", isChecked: false },
+      { text: "어린 시절 친구", isChecked: false },
+      { text: "초등학교 때 좋아했던 과목", isChecked: false },
+      { text: "어린 시절의 꿈", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "여행",
+    contents: [
+      { text: "가장 기억에 남는 여행지", isChecked: false },
+      { text: "여행 중 겪은 에피소드", isChecked: false },
+      { text: "다음 여행지 계획", isChecked: false },
+      { text: "여행 스타일", isChecked: false },
+      { text: "좋아하는 여행 동반자", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "음식",
+    contents: [
+      { text: "좋아하는 음식", isChecked: false },
+      { text: "싫어하는 음식", isChecked: false },
+      { text: "특별한 식사 경험", isChecked: false },
+      { text: "가장 좋아하는 음식점", isChecked: false },
+      { text: "다음에 시도해보고 싶은 요리", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "영화와 드라마",
+    contents: [
+      { text: "최근 본 영화", isChecked: false },
+      { text: "가장 좋아하는 배우", isChecked: false },
+      { text: "영화 추천", isChecked: false },
+      { text: "가장 감동적인 영화 장면", isChecked: false },
+      { text: "좋아하는 드라마 시리즈", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "취미와 여가",
+    contents: [
+      { text: "즐기는 취미", isChecked: false },
+      { text: "최근에 시작한 취미", isChecked: false },
+      { text: "취미로 배운 기술", isChecked: false },
+      { text: "주말 여가 활동", isChecked: false },
+      { text: "함께 하고 싶은 취미", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "음악",
+    contents: [
+      { text: "좋아하는 음악 장르", isChecked: false },
+      { text: "가장 좋아하는 음악가", isChecked: false },
+      { text: "콘서트 경험", isChecked: false },
+      { text: "최근에 재생 목록에서 가장 많이 들은 노래", isChecked: false },
+      { text: "음악과 관련된 추억", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "일과 경력",
+    contents: [
+      { text: "현재 직업", isChecked: false },
+      { text: "직업 선택의 이유", isChecked: false },
+      { text: "직장에서의 일상", isChecked: false },
+      { text: "직업에서의 성취", isChecked: false },
+      { text: "미래의 경력 계획", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "문화와 예술",
+    contents: [
+      { text: "가장 인상 깊었던 전시회", isChecked: false },
+      { text: "문화적으로 영향을 받은 것", isChecked: false },
+      { text: "좋아하는 예술가", isChecked: false },
+      { text: "최근에 방문한 박물관", isChecked: false },
+      { text: "예술에 대한 개인적인 의견", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "패션과 스타일",
+    contents: [
+      { text: "개인의 패션 스타일", isChecked: false },
+      { text: "가장 좋아하는 의상", isChecked: false },
+      { text: "패션에 영향을 준 사람", isChecked: false },
+      { text: "최근에 구입한 패션 아이템", isChecked: false },
+      { text: "패션 관련 좋아하는 브랜드", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "가족과 관계",
+    contents: [
+      { text: "가족 구성원", isChecked: false },
+      { text: "어린 시절 가족과의 추억", isChecked: false },
+      { text: "가족과의 특별한 전통", isChecked: false },
+      { text: "가족과 함께 보내는 시간", isChecked: false },
+      { text: "가족에 대한 소중한 생각", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "영감을 주는 인물",
+    contents: [
+      { text: "영향을 받은 인물", isChecked: false },
+      { text: "만나보고 싶은 역사적 인물", isChecked: false },
+      { text: "존경하는 현대 인물", isChecked: false },
+      { text: "인생 조언을 얻고 싶은 인물", isChecked: false },
+      { text: "인상 깊은 유명 인용구", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "여가와 휴식",
+    contents: [
+      { text: "좋아하는 휴식 방법", isChecked: false },
+      { text: "최근에 방문한 재미있는 장소", isChecked: false },
+      { text: "휴가 계획", isChecked: false },
+      { text: "휴식을 통해 얻는 것", isChecked: false },
+      { text: "취미와 휴식의 균형", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "책과 문학",
+    contents: [
+      { text: "최근 읽은 책", isChecked: false },
+      { text: "영향력 있는 작가", isChecked: false },
+      { text: "문학 속 좋아하는 캐릭터", isChecked: false },
+      { text: "다시 읽고 싶은 책", isChecked: false },
+      { text: "추천하고 싶은 시", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+  {
+    topic: "건강과 웰빙",
+    contents: [
+      { text: "건강 유지 비법", isChecked: false },
+      { text: "스트레스 관리 방법", isChecked: false },
+      { text: "즐겨하는 건강식", isChecked: false },
+      { text: "건강을 위해 시작한 활동", isChecked: false },
+      { text: "웰빙을 위한 일상의 팁", isChecked: false },
+    ],
+    isDisplayed: false,
+  },
+];
--- src/pages/ice-breaking-game/random-game/RandomPage.js
+++ src/pages/ice-breaking-game/random-game/RandomPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { calculateMainLayoutHeight } from "styles/functions";
+import { someItems } from "apis/origin-data/ice-breaking-game/random-game/some-items";
 
 const Container = styled.div`
   display: flex;
@@ -26,21 +27,34 @@ const Title = styled.div`
   font-size: ${({ fontSize }) => fontSize || "16px"};
 `;
 
-const MessageWrapper = styled.div`
+const Topic = styled.div`
+  color: ${({ theme, color }) => color || theme.colors.secondary};
+`;
+
+const ContentsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
   padding: 20px;
+  font-family: "Poor Story";
+  font-size: 24px;
 `;
 
-const Message = styled.div`
+const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   text-align: center;
-  font-size: 24px;
   width: 100%;
-  font-family: "Poor Story";
+  border: solid 1px black;
+  border-radius: 4px;
+  padding: 10px;
+  margin: 2px 0;
+  background-color: ${({ isChecked }) => (isChecked ? "black" : "white")};
+  color: ${({ isChecked }) => (isChecked ? "darkgray" : "black")};
+  text-decoration: ${({ isChecked }) => (isChecked ? "line-through" : "none")};
+  cursor: pointer;
 `;
 
 const ripple = keyframes`
@@ -64,21 +78,28 @@ const Ripple = styled.div`
   height: 50px;
 `;
 
+const getRandomContentIndex = (items) => {
+  const availableContents = items.filter((item) => !item.isDisplayed);
+  const randomIndex = Math.floor(Math.random() * availableContents.length);
+  return items.indexOf(availableContents[randomIndex]);
+};
+
 const RandomPage = () => {
-  const [currentMessageIndex, setCurrentMessageIndex] = useState(
-    getRandomMessageIndex(messages)
+  const [items, setItems] = useState(someItems);
+  const [currentContentIndex, setCurrentContentIndex] = useState(
+    getRandomContentIndex(items)
   );
   const [ripples, setRipples] = useState([]);
 
-  const handleClick = (event) => {
-    if (currentMessageIndex >= 0) {
-      messages[currentMessageIndex].isDisplayed = true;
-    } else {
-      messages.map((message) => (message.isDisplayed = false));
+  const handleClickContainer = (event) => {
+    const newIndex = getRandomContentIndex(items);
+    if (newIndex === -1) {
+      for (const item of items) {
+        item.isDisplayed = false;
+        item.contents.forEach((content) => (content.isChecked = false));
+      }
     }
-
-    const newIndex = getRandomMessageIndex(messages);
-    setCurrentMessageIndex(newIndex);
+    setCurrentContentIndex(newIndex);
 
     const x = event.clientX;
     const y = event.clientY;
@@ -90,27 +111,53 @@ const RandomPage = () => {
       setRipples((oldRipples) =>
         oldRipples.filter((ripple) => ripple.id !== id)
       );
-    }, 1000); // 1초 후에 물방울 제거
+    }, 1000);
+  };
+
+  const handleContentClick = (event, contentIndex) => {
+    event.stopPropagation();
+    const newItems = [...items];
+    const currentItem = newItems[currentContentIndex];
+    currentItem.contents[contentIndex].isChecked =
+      !currentItem.contents[contentIndex].isChecked;
+
+    // contents의 isChecked가 모두 true이면 isDisplayed를 false로 하여, 다신 노출이 안되도록 한다.
+    if (currentItem.contents.every((content) => content.isChecked)) {
+      currentItem.isDisplayed = true;
+    }
+
+    setItems(newItems);
   };
 
   return (
-    <Container onClick={handleClick}>
+    <Container onClick={handleClickContainer}>
       <TitleWrapper>
         <Title color="black" fontSize="13px">
-          화면을 터치하면 주제가 변경됩니다.
+          빈 화면을 터치하면 주제가 변경됩니다.
         </Title>
         <Title fontWeight="bold">경청과 리액션을 충분히 해주세요.</Title>
         <Title fontWeight="bold">
           꼬리를 이은 질문은 분위기를 좋게 만들어줍니다.
         </Title>
       </TitleWrapper>
-      <MessageWrapper>
-        {currentMessageIndex >= 0
-          ? messages[currentMessageIndex].message
-              .split("\n")
-              .map((a) => <Message>{a}</Message>)
-          : "주제가 소진되었어요. 다시 시작할게요."}
-      </MessageWrapper>
+
+      {/* <Topic>{items[currentContentIndex].topic}</Topic> */}
+      <ContentsWrapper>
+        {currentContentIndex >= 0 ? (
+          items[currentContentIndex].contents.map((content, index) => (
+            <Content
+              key={index}
+              isChecked={content.isChecked}
+              onClick={(e) => handleContentClick(e, index)}
+            >
+              {content.text}
+            </Content>
+          ))
+        ) : (
+          <Content>주제가 소진되었어요. 다시 시작할게요.</Content>
+        )}
+      </ContentsWrapper>
+
       {ripples.map((ripple) => (
         <Ripple
           key={ripple.id}
@@ -119,117 +166,6 @@ const RandomPage = () => {
       ))}
     </Container>
   );
-};
-
-const messages = [
-  {
-    message:
-      "좋아하는 음식이 뭐에요?\n최근에 가장 맛있었던 메뉴 추천해주세요.\n자주 가는 식당있으세요?",
-    isDisplayed: false,
-  },
-  {
-    message:
-      "여행 좋아하세요?\n최근에 간 여행지는 어디인가요?\n국내 여행은 좋아하시나요?",
-    isDisplayed: false,
-  },
-  {
-    message: "취미가 뭐에요?\n새로운 취미에 도전하는 것을 즐기시나요?",
-    isDisplayed: false,
-  },
-  {
-    message:
-      "최근 본 영화나 드라마는?\n가장 좋아하는 배우가 누구에요?\n가장 좋아하는 장르는?",
-    isDisplayed: false,
-  },
-  {
-    message:
-      "좋아하는 음악은?\n좋아하는 음악 장르는?\n좋아하는 가수는?\n콘서트 가는 것도 즐기시나요?",
-    isDisplayed: false,
-  },
-  {
-    message:
-      "운동 좋아하시나요?\n즐겨하는 운동은?\n운동 루틴이 있으신가요?\n스포츠 경기를 직접 보러도 가시나요?",
-    isDisplayed: false,
-  },
-  {
-    message:
-      "독서 좋아하시나요?\n최근에 읽은 책이 뭐에요?\n추천해주실 만한 책 있으신가요?\n좋아하는 작가가 있으신가요?",
-    isDisplayed: false,
-  },
-  {
-    message:
-      "강아지/고양이 좋아하시나요?\n강아지/고양이 중에 어느 걸 더 선호하시나요?\n키우시는 반려동물 있으신가요?\n동물원/수족관은 즐겨 가는 편이신가요?",
-    isDisplayed: false,
-  },
-  {
-    message:
-      "커피 좋아하시나요?\n분좋카(분위기 좋은 카페) 가는 것 좋아하시나요?\n가 본 카페 중 가장 괜찮았던 곳은\n자주 즐겨드시는 커피나 음료는 무엇인가요?",
-    isDisplayed: false,
-  },
-  {
-    message: "패션: 스타일, 옷 쇼핑 이야기, 좋아하는 브랜드",
-    isDisplayed: false,
-  },
-  {
-    message: "계절: 좋아하는 계절, 계절별 활동, 계절 음식",
-    isDisplayed: false,
-  },
-  {
-    message: "학창 시절: 학교 생활, 학창 시절 추억, 동창회",
-    isDisplayed: false,
-  },
-  {
-    message: "자기 계발: 배우고 싶은 것, 새로운 도전, 자기 계발 방법",
-    isDisplayed: false,
-  },
-  { message: "문화: 전통 문화, 문화 행사, 전시회 관람", isDisplayed: false },
-  {
-    message: "요리: 직접 해 본 요리, 요리 레시피 공유, 요리 팁",
-    isDisplayed: false,
-  },
-  {
-    message: "사회 이슈: 최근 뉴스, 사회적 이슈에 대한 생각, 토론",
-    isDisplayed: false,
-  },
-  { message: "미래 계획: 장래 희망, 미래 목표, 인생 계획", isDisplayed: false },
-  { message: "가족: 가족 이야기, 형제 자매, 가족 모임", isDisplayed: false },
-  {
-    message: "취직/직장 생활: 직장 이야기, 일상 업무, 직장 생활 팁",
-    isDisplayed: false,
-  },
-  { message: "건강: 건강 관리 방법, 운동 습관, 건강 식단", isDisplayed: false },
-  { message: "경제: 경제 상황, 투자 이야기, 재테크 팁", isDisplayed: false },
-  {
-    message: "교육: 교육 경험, 배우고 싶은 것, 교육 관련 이야기",
-    isDisplayed: false,
-  },
-  { message: "사진: 사진 찍기, 사진 공유, 사진 관련 팁", isDisplayed: false },
-  { message: "자연: 자연 경관, 자연 탐방, 캠핑 이야기", isDisplayed: false },
-  {
-    message: "자전거: 자전거 타기, 자전거 여행, 자전거 관련 팁",
-    isDisplayed: false,
-  },
-  {
-    message: "자동차: 좋아하는 자동차, 운전 경험, 자동차 여행",
-    isDisplayed: false,
-  },
-  {
-    message: "봉사활동: 봉사활동 경험, 봉사활동 추천, 사회 공헌",
-    isDisplayed: false,
-  },
-  { message: "기술: 최신 기술, IT 관련 이야기, 기술 발전", isDisplayed: false },
-  { message: "게임: 좋아하는 게임, 게임 경험, 게임 추천", isDisplayed: false },
-  {
-    message: "유머: 재미있는 이야기, 유머 감각, 웃긴 에피소드",
-    isDisplayed: false,
-  },
-];
-
-const getRandomMessageIndex = (messages) => {
-  const availableMessages = messages.filter((msg) => !msg.isDisplayed);
-  if (availableMessages.length === 0) return -1;
-  const randomIndex = Math.floor(Math.random() * availableMessages.length);
-  return messages.indexOf(availableMessages[randomIndex]);
 };
 
 export default RandomPage;
